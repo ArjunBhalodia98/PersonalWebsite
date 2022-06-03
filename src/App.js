b932ea5958d22924d8a2793591f4d4074584bfbf
Arjun Bhalodia - Mac <thisajplays@gmail.com>
2022-06-03 09:56:17 -0400

    deleted node modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import Background_Image from './Assests/Background_Image.jpg';
+import styled from "styled-components";
+import "./App.css";
+import Typed from 'react-typed';
 
 function App() {
+
+    const StyledDiv = styled.div`
+      background-image: url(${Background_Image});
+      background-size: cover;
+      background-repeat: no-repeat;
+      //opacity: 0.5;
+      position: relative;
+      height: 100vh;
+      @media only screen and (max-width: 820px){
+        background-position: center;
+      }
+    `;
+
+    const BodyDiv = styled.div`
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    `;
+
+    const StyledH1 = styled.h1`
+      line-heright: 1.5;
+      letter-spacing: 1.5;
+      font-family: "Nunito";
+    `;
+
+    const StyledH3 = styled.h3`
+      line-heright: 1.5;
+      letter-spacing: 1.15;
+      font-family: "Lato";
+      font-size: 20px;
+    `;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <StyledDiv/>
+        <BodyDiv>
+            <StyledH3>Hey, I'm</StyledH3>
+            <StyledH1>Arjun Bhalodia</StyledH1>
+            <Typed
+                strings={["Backend Engineer", "Full Stack Engineeer"]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop
+            />
+        </BodyDiv>
     </div>
   );
 }
